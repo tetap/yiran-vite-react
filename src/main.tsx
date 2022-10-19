97eb6f8dbba8db55375d55ae1ffaa7d6bf31cad4
zyc <239559238@qq.com>
2022-10-19 10:21:04 +0800

    ✨ feat: 添加公共样式

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { useLocale } from '@/locale'
+import './theme/global.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
